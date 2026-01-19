529d7a759de7e51dee5ace553d88d89478504f72
Cllspy <ifal.edu.2017@gmail.com>
2026-01-19 15:57:52 -0300

    add: socket_event()

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+import random
+import requests
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
 users = {}
+
+@socketio.on("connect")
+def socket_event():
+    prefix = "User_" 
+    random_id = random.randint(1000, 9999)
+    username = prefix + str(random_id)
+    gender = random.choice(["boy", "girl"])
+    avatar_url = requests.get(f"https://avatar.iran.liara.run/public/{gender}?username={username}")
+    users["request.sid"] = username
 
 @app.route("/")
 def hello_world():
---
+++ static/main.js
@@ -0,0 +1,7 @@
+const socket = socketIO();
+
+nameArea = document.getElementById("name-area");
+inputArea = document.getElementById("input-area");
+
+updateUser = document.getElementById("update-user-name");
+sendBtn = document.getElementById("send-btn");
