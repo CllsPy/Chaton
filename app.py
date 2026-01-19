c664ceaba604ba3cadcb74fd2339ae1837897274
Cllspy <ifal.edu.2017@gmail.com>
2026-01-19 16:28:51 -0300

    add: user_join, set_user

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+from flask import request
 import random
-import requests
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -14,8 +14,11 @@ def socket_event():
     random_id = random.randint(1000, 9999)
     username = prefix + str(random_id)
     gender = random.choice(["boy", "girl"])
-    avatar_url = requests.get(f"https://avatar.iran.liara.run/public/{gender}?username={username}")
-    users["request.sid"] = username
+    avatar_url = f"https://avatar.iran.liara.run/public/{gender}?username={username}"
+    users[request.sid] = {"name": username, "avatar": avatar_url} 
+    socketio.emit('user_joined', users[request.sid], broadcast=True);
+    socketio.emit('set_username', users[request.sid]);
+    
 
 @app.route("/")
 def hello_world():
