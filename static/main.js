c0a438629ab05e32698bf67bc7670d770df4619a
Cllspy <ifal.edu.2017@gmail.com>
2026-01-20 16:21:52 -0300

    bug: io() undefield

--- static/main.js
+++ static/main.js
@@ -1,7 +1,29 @@
-const socket = socketIO();
+const socket = io();
 
-nameArea = document.getElementById("name-area");
-inputArea = document.getElementById("input-area");
+const nameArea = document.getElementById("name-area");
+const inputArea = document.getElementById("input-area");
 
-updateUser = document.getElementById("update-user-name");
-sendBtn = document.getElementById("send-btn");
+const updateUser = document.getElementById("update-user-name");
+const sendBtn = document.getElementById("send-btn");
+
+const chatMessages = document.querySelector(".chat-messages");
+
+socket.on("user_joined", (data) => {
+  //atualizar HTML
+  const username = data.name;
+  const msg = document.createElement("div");
+  msg.classList.add("system-message"); // opcional, para diferenciar
+  msg.textContent = `[${username}] joined the chat`;
+
+  // adicionar no chat
+  chatMessages.appendChild(msg);
+
+  // rolar o chat para o final (opcional)
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+socket.on("set_username", (data) => {
+  //atualizar HTML
+  const username = data.name;
+  nameArea.textContent = `Você é ${username}`;
+});
